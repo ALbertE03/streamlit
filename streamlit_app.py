15d0c021afccf7f647a4ed0d86649f0dec6ebbb7
ALbertEO3 <marichalalberto292@gmail.com>
2024-06-02 19:52:18 -0400

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,23 +16,35 @@ with st.form("inscripcion"):
     enviado = st.form_submit_button("Enviar")
 
     if enviado:
-        if edad < 15:
-            st.error(
-                f"Lo sentimos {nombre}, debes tener al menos 15 años para inscribirte."
-            )
+        if nombre != "" and apellidos != "" and edad > 15 and municipio != "":
+            st.success(f"¡Gracias por inscribirte, {nombre} {apellidos}!")
+            st.write(f"Edad: {edad}")
+            st.write(f"Municipio: {municipio}")
+            st.write(f"Motivos: {motivos}")
 
+        if nombre == "":
+            st.error("Por favor, ingresa tu nombre")
+        if apellidos == "":
+            st.error("Por favor ingresa tus apellidos")
+        if edad < 15 and edad != 1:
+            st.error(
+                f"lo sentimos {nombre} debes tener al menos 15 años para inscribirte."
+            )
+        if edad == 1:
+            st.error("Por favor modifique el campo edad con su edad real")
+        if municipio == "":
+            st.error("Por favor ingresa tu municipio de residencia")
         elif nombre.lower() in baneados:
             st.error(
                 f"Lo sentimos {nombre}, no puedes inscribirte. Fuiste baneado del Gym el mes pasado"
             )
-        elif 60 <= edad <= 70:
-            st.warning("Tiene una edad avanzada segura que quiere inscribirse al GYM?")
-            st.success(f"¡Gracias por inscribirte, {nombre} {apellidos}!")
-            st.write(f"Edad: {edad}")
-            st.write(f"Municipio: {municipio}")
-            st.write(f"Motivos: {motivos}")
-        else:
-            st.success(f"¡Gracias por inscribirte, {nombre} {apellidos}!")
-            st.write(f"Edad: {edad}")
-            st.write(f"Municipio: {municipio}")
-            st.write(f"Motivos: {motivos}")
+        if (
+            motivos == ""
+            and nombre != ""
+            and apellidos != ""
+            and edad > 15
+            and municipio != ""
+        ):
+            st.warning(
+                "no quiere poner ningún motivo 🙂? o es el que todos sabemos 😉?"
+            )
